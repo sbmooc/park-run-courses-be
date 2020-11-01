ce96fa7df467df18dd156fcc54e853f20c0c5af0
oli m-c <olster98@gmail.com>
2020-11-01 11:58:10 +0000

    Helper function to convert string to array of ints for coordinates

--- geoJsonhelpers.js
+++ geoJsonhelpers.js
@@ -11,17 +11,45 @@ const latLngToLinestring = (latLng) => {
 const assertArrayCoordinatesLooksValid = (coordinates) => {
     assert(coordinates.length > 0)
     coordinates.forEach(coordinate => {
-        if (coordinate.length != 2){
-            throw 'Cordinates array does not appear valid'
+        if (coordinate.length != 2) {
+            throw 'Coordinates array does not appear valid'
         }
     })
 }
 
-const convertDbObject = () => {
-    console.log('hello')
+const convertDbObjects = (dbObjects) => {
+    const geoJson = {
+        type: 'FeatureCollection',
+        features: []
+    }
+    dbObjects.forEach(dbObject => {
+        geoJson.features.push(
+            {
+                type: 'Feature',
+                geometry: latLngToLinestring(dbObject.latLng),
+                properties: {
+                    name: dbObject.name,
+                    id: dbObject.id,
+                }
+            }
+        )
+    })
+    return geoJson
+}
+
+const convertLatLngString = (latLngString) => {
+    newLatLng = []
+    latLngString.split('|').forEach(
+        coordiante => {
+            const res = coordiante.split(',')
+            newLatLng.push(res.map(str => parseInt(str)))
+        }
+    )
+    return newLatLng
 }
 
 module.exports = {
     latLngToLinestring,
-    convertDbObject
+    convertDbObjects,
+    convertLatLngString
 }
--- tests/geoJsonhelpers.test.js
+++ tests/geoJsonhelpers.test.js
@@ -1,4 +1,4 @@
-const { latLngToLinestring, convertDbObject } = require('../geoJsonhelpers')
+const { latLngToLinestring, convertDbObjects, convertLatLngString } = require('../geoJsonhelpers')
 
 const validGeoJsonGeometry = {
     type: 'LineString',
@@ -15,38 +15,49 @@ const testCourse = {
     name: 'TestCourse',
     id: 1,
     eventId: 2,
-    latLng: [
-        [1, 2], [2, 3], [3, 4], [4, 5]
-    ]
+    latLng: "1,2|2,3|3,4|4,5"
+    
 }
 
 const testCourseCollection = [
-{
-    name: 'TestCourse1',
-    id: 1,
-    eventId: 2,
-    latLng: [
-        [1, 2], [2, 3], [3, 4], [4, 5]
-    ]
-},
-{
-    name: 'TestCourse2',
-    id: 2,
-    eventId: 2,
-    latLng: [
-        [1, 2], [2, 3], [3, 4], [4, 5]
-    ]
-},
-{
-    name: 'TestCourse3',
-    id: 3,
-    eventId: 2,
-    latLng: [
-        [1, 2], [2, 3], [3, 4], [4, 5]
-    ]
-}
+    {
+        name: 'TestCourse1',
+        id: 1,
+        eventId: 2,
+        latLng: [
+            [1, 2], [2, 3], [3, 4], [4, 5]
+        ]
+    },
+    {
+        name: 'TestCourse2',
+        id: 2,
+        eventId: 2,
+        latLng: [
+            [1, 2], [2, 3], [3, 4], [4, 5]
+        ]
+    },
+    {
+        name: 'TestCourse3',
+        id: 3,
+        eventId: 2,
+        latLng: [
+            [1, 2], [2, 3], [3, 4], [4, 5]
+        ]
+    }
 ]
 
+const validGeoJsonOneCourse = {
+    type: 'FeatureCollection',
+    features: [{
+        geometry: {
+            type: 'LineString',
+            coordinates: [[1,2], [2,3], [3, 4], [4, 5]]
+        }
+    }
+    ]
+}
+
+latLngString = "1,2|2,3|3,4|4,5"
 validLatLngArray = [[1, 2], [2, 3], [3, 4], [4, 5]]
 invalidLatLngArray = [[1, 2], [2, 3, 4], [3, 4], [4, 5]]
 
@@ -58,10 +69,14 @@ test('expect an invalid LatLng array to throw an error', () => {
     expect(() => latLngToLinestring(invalidLatLngArray)).toThrow();
 });
 
+test('convert a latLng string into an array of coordinates', () => {
+    expect(convertLatLngString(latLngString)).toEqual(validLatLngArray);
+})
+
 test('convert a Course object GeoJson', () => {
-    expect(convertDbObject(testCourse).toEqual(validGeoJsonOneCourse))
+    expect(convertDbObjects([testCourse])).toEqual(validGeoJsonOneCourse)
 })
 
 test('convert multiple Course objects to valid GeoJson', () => {
-    expect(convertDbObject(testCourseCollection).toEqual(validGeoJsonThreeCourses))
+    expect(convertDbObjects(testCourseCollection)).toEqual(validGeoJsonThreeCourses)
 })
